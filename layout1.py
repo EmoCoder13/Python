8ca77bde9a88e22c00ff81744240755063e4853d
EmoCoder13 <43414416+EmoCoder13@users.noreply.github.com>
2018-10-18 22:24:32 +0800

    Update layout1.py
    Semi-working version, apply to display images on qlabel but very very lag. No visual box boundaries but able to detect based on terminal output

--- layout1.py
+++ layout1.py
@@ -1,4 +1,16 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
 import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import qimage2ndarray
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
 
 # import some PyQt5 modules
 from PyQt5.QtWidgets import QApplication
@@ -12,6 +24,66 @@ import cv2
 
 from ui_main_window import *
 
+#object detection imports
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+sys.path.append(r"C:\Users\yan_h\Documents\GitHub\models\research\object_detection")
+
+# What model to download.
+MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+# Download Model
+
+# In[5]:
+
+opener = urllib.request.URLopener()
+opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+tar_file = tarfile.open(MODEL_FILE)
+for file in tar_file.getmembers():
+  file_name = os.path.basename(file.name)
+  if 'frozen_inference_graph.pb' in file_name:
+    tar_file.extract(file, os.getcwd())
+
+
+# ## Load a (frozen) Tensorflow model into memory.
+
+# In[6]:
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+
+# ## Loading label map
+# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+
+# In[7]:
+
+label_map = label_map_util.load_labelmap(os.path.join(r'C:\Users\yan_h\Documents\GitHub\models\research\object_detection\data', 'mscoco_label_map.pbtxt'))
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+
 class MainWindow(QWidget):
     # class constructor
     def __init__(self):
@@ -22,13 +94,27 @@ class MainWindow(QWidget):
 
         # create a timer
         self.timer = QTimer()
+        self.timer.setTimerType(0)
         # set timer timeout callback function
         self.timer.timeout.connect(self.viewCam)
         # set control_bt callback clicked  function
         self.ui.control_bt.clicked.connect(self.controlTimer)
 
+    def convertQImageToMat(self,incomingImage):
+
+        incomingImage = incomingImage.convertToFormat(3)
+
+        width1 = incomingImage.width()
+        height1 = incomingImage.height()
+
+        ptr = incomingImage.bits()
+        ptr.setsize(incomingImage.byteCount())
+        arr = np.array(ptr).reshape(1,height1, width1, 3)  #  Copies the data
+        return arr
+
     # view camera
     def viewCam(self):
+
         # read image in BGR format
         ret, image = self.cap.read()
         # convert image to RGB format
@@ -37,9 +123,49 @@ class MainWindow(QWidget):
         height, width, channel = image.shape
         step = channel * width
         # create QImage from image
-        qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
+        qImg = QImage(image.data, width, height, step, QImage.Format_ARGB32)
         # show image in img_label
         self.ui.image_label.setPixmap(QPixmap.fromImage(qImg))
+
+        with detection_graph.as_default():
+            with tf.Session(graph=detection_graph) as sess:
+
+                    imageCon = qimage2ndarray.rgb_view(qImg)
+                    #inserted code from capture.py
+                    image_np_expanded = np.expand_dims(qImg, axis=0)
+                    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+                    # Each box represents a part of the image where a particular object was detected.
+                    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+                    # Each score represent how level of confidence for each of the objects.
+                    # Score is shown on the result image, together with the class label.
+                    scores = detection_graph.get_tensor_by_name('detection_scores:0')
+                    classes = detection_graph.get_tensor_by_name('detection_classes:0')
+                    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+                    # Actual detection.
+                    (boxes, scores, classes, num_detections) = sess.run(
+                        [boxes, scores, classes, num_detections],
+                        feed_dict={image_tensor: image_np_expanded})
+
+                    threshold = 0.5
+                    objects = []
+                    for index, value in enumerate(classes[0]):
+                        object_dict = {}
+                        if scores[0,index] > threshold:
+                            object_dict = [(category_index.get(value).get('name'))]
+                            objects.append(object_dict)
+                    print (objects)
+
+                    # Visualization of the results of a detection.
+                    vis_util.visualize_boxes_and_labels_on_image_array(
+                        image,
+                        np.squeeze(boxes),
+                        np.squeeze(classes).astype(np.int32),
+                        np.squeeze(scores),
+                        category_index,
+                        use_normalized_coordinates=True,
+                        line_thickness=8)
+
+                    #end of insert
 
     # start/stop timer
     def controlTimer(self):
@@ -48,7 +174,7 @@ class MainWindow(QWidget):
             # create video capture
             self.cap = cv2.VideoCapture(0)
             # start timer
-            self.timer.start(20)
+            self.timer.start(10)
             # update control_bt text
             self.ui.control_bt.setText("Stop")
         # if timer is started
